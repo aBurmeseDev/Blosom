da843b5f7e4ca37781c4cb12f4046b3d2ab228c8
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-09-10 22:25:21 -0700

    change app as react component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import { Router, Switch } from 'react-router-dom';
@@ -7,14 +7,15 @@ import NavBar from './pages/Navbar';
 import Home from './pages/Home';
 import Order from './pages/Order';
 
-function App() {
-  return (
-    <div className='App'>
-      <NavBar />
-      <Home />
-      <Order />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <NavBar />
+        <Home />
+        <Order />
+      </div>
+    );
+  }
 }
-
 export default App;
