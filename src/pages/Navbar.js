c3c2afb5656140ce865521fa3ef25cf5c2794459
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-09-10 21:44:23 -0700

    add nav items and import reactstraps

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,4 +1,44 @@
 import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
 
-class Navbar extends Component {}
+class NavBar extends Component {
+  render() {
+    return (
+      <>
+        <Navbar>
+          <NavbarBrand href='/'>Blosom Bento</NavbarBrand>
+          <NavbarToggler onClick={this.toggle.bind(this)} />
+          <Collapse>
+            <Nav className='ml-auto' navbar>
+              <NavItem>
+                <NavLink href='#'>Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='#'>Menu</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='#'>Order</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='#'>About</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </>
+    );
+  }
+}
 export default Navbar;
