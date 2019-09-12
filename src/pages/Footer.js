0641a615aaa64259fd3fa77c6770703b2cea3dd4
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-09-11 22:27:58 -0700

    add footer

---
+++ src/pages/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className='Footer'>
+        <h4>this is footer</h4>
+      </div>
+    );
+  }
+}
