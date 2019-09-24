96a766b5205642950933968857e33c071e5792ca
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-09-23 19:24:08 -0700

    edit menu section

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Menu extends Component {
   render() {
     return (
-      <div>
+      <div className='Menu'>
         <h5>this is menu section</h5>
       </div>
     );
