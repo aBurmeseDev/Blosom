28eb1a842c953ca7947124d63f6b0b9ebba24c98
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-09-28 12:10:21 -0700

    add images to gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
 
+import { Row, Col } from 'reactstrap';
+
+import img1 from '../menu_items/Avocado Roll.jpg';
+
 export default class Gallery extends Component {
   render() {
-    return <div className='Gallery' />;
+    return (
+      <div className='Gallery'>
+        <div id='gallery-body'>
+          <Row>
+            <Col xs='12' md='2'>
+              <img src={img1} className='gallery-img' />
+            </Col>
+            <Col xs='12' md='2'>
+              <img src={img1} className='gallery-img' />
+            </Col>
+            <Col xs='12' md='2'>
+              <img src={img1} className='gallery-img' />
+            </Col>
+            <Col xs='12' md='2'>
+              <img src={img1} className='gallery-img' />
+            </Col>
+            <Col xs='12' md='2'>
+              <img src={img1} className='gallery-img' />
+            </Col>
+            <Col xs='12' md='2'>
+              <img src={img1} className='gallery-img' />
+            </Col>
+          </Row>
+        </div>
+      </div>
+    );
   }
 }
