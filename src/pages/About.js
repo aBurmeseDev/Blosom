4cac074d90f53074b7d8b96b5c572baff6151048
John Lwin <47447266+aBurmeseDev@users.noreply.github.com>
2019-11-08 22:22:57 -0800

    edit about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,45 +8,28 @@ export default class About extends Component {
         <div id='about-body'>
           <Row>
             <Col xs='12' md='4' style={{ paddingTop: '12px' }}>
-              <h4>location</h4>
+              <h5>location</h5>
               <span>
                 735 E. 12th st. suite #411 <br /> Los Angeles, CA 90021
               </span>
             </Col>
             <Col xs='12' md='4' style={{ paddingTop: '12px' }}>
-              <h4>hours</h4>
+              <h5>hours</h5>
               <span>M-F 9:00am - 5:00pm</span> <br />
               Sat 7:30am - 12:30pm
             </Col>
             <Col xs='12' md='4' style={{ paddingTop: '12px' }}>
-              <h4>contact</h4>
+              <h5>contact</h5>
               <span>T: 213 746 4642</span> <br />
-              <a href='#'>
+              <a href='https://www.facebook.com/pages/category/Fast-Food-Restaurant/Blosom-Bento-194429073910986/'>
                 <i className='fab fa-facebook fa-2x' />
               </a>
-              <a href='#'>
+              <a href='https://www.yelp.com/biz/blosom-bento-los-angeles'>
                 <i className='fab fa-yelp fa-2x' />
               </a>
             </Col>
           </Row>
         </div>
-        {/* <div id='deliver-body'>
-          <h5 id='order'>Delivering Areas</h5>
-          <Row>
-            <Col xs='12' md='3'>
-              <h6>Within 0.5 mile</h6> <strong>FREE</strong>
-            </Col>
-            <Col xs='12' md='3'>
-              <h6>Within 1 mile</h6>
-            </Col>
-            <Col xs='12' md='3'>
-              <h6>Within 3 mile</h6>
-            </Col>
-            <Col xs='12' md='3'>
-              <h6>Over mile</h6>
-            </Col>
-          </Row>
-        </div> */}
       </div>
     );
   }
