97eef8df5e872e25be8c537595252ecf0a53fd8e
John Lwin <47447266+aBurmeseDev@users.noreply.github.com>
2019-11-19 23:00:36 -0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
   render() {
-    return <div className='para' />;
+    return (
+      <div className='para'>
+        <h4 className='welcome'>Welcome</h4>
+      </div>
+    );
   }
 }
