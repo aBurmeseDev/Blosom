262b14977862a8610b114e0b6912bb4a0bb4931b
John Lwin <47447266+aBurmeseDev@users.noreply.github.com>
2019-11-19 23:00:17 -0800

    Update Order.js

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -9,76 +9,10 @@ export default class Order extends Component {
   render() {
     return (
       <div className='OrderInfo'>
-        <div id='order-body'>
-          <h3>order online</h3>
-          <div style={{ height: '24px' }} />
-
-          <Row>
-            <Col xs='12' md='3'>
-              <Button
-                className='orderBtn'
-                outline
-                color='light'
-                href='https://www.ubereats.com/en-US/'
-              >
-                <img
-                  src={ubereats}
-                  alt='order UberEats'
-                  style={{ height: '1rem' }}
-                />
-              </Button>
-            </Col>
-            <Col xs='12' md='3'>
-              <Button
-                className='orderBtn'
-                outline
-                color='light'
-                href='https://get.chownow.com/'
-              >
-                <img
-                  src={chownow}
-                  alt='order ChowNow'
-                  style={{ height: '2.95rem' }}
-                />
-              </Button>
-            </Col>
-            <Col xs='12' md='3'>
-              <Button
-                className='orderBtn'
-                outline
-                color='light'
-                href='https://www.grubhub.com/'
-              >
-                <img
-                  src={grubhub}
-                  alt='order GrubHub'
-                  style={{ height: '2.95rem' }}
-                />
-              </Button>
-            </Col>
-            <Col xs='12' md='3'>
-              <Button
-                className='orderBtn'
-                outline
-                color='light'
-                href='https://www.doordash.com/'
-              >
-                <img
-                  src={doordash}
-                  alt='order DoorDash'
-                  style={{ height: '2rem' }}
-                />
-              </Button>
-            </Col>
-            {/* <Col xs='12' md='3'>
-              <Button outline color='info'>
-                DOORDASH
-              </Button>
-            </Col> */}
-          </Row>
-        </div>
+        <div style={{ height: '24px' }} />
         <div id='deliver-body'>
-          <h3>delivering areas</h3>
+          <h4>Delivering Areas</h4>
+
           <Row>
             <Col xs='12' md='2' />
             <Col xs='12' md='2'>
@@ -97,6 +31,75 @@ export default class Order extends Component {
               <p>Please call us and inquire within.</p>
             </Col>
             <Col xs='12' md='2' />
+          </Row>
+        </div>
+        <div id='order-body'>
+          <Row>
+            <Col xs='12' md='3'>
+              <Button
+                className='orderBtn'
+                outline
+                color='light'
+                target='_blank'
+                href='https://get.chownow.com/'
+              >
+                <img
+                  src={chownow}
+                  alt='order ChowNow'
+                  style={{ height: '2.95rem' }}
+                />
+              </Button>
+            </Col>
+            <Col xs='12' md='3'>
+              <Button
+                className='orderBtn'
+                outline
+                color='light'
+                target='_blank'
+                href='https://www.ubereats.com/en-US/'
+              >
+                <img
+                  src={ubereats}
+                  alt='order UberEats'
+                  style={{ height: '1rem' }}
+                />
+              </Button>
+            </Col>
+            <Col xs='12' md='3'>
+              <Button
+                className='orderBtn'
+                outline
+                color='light'
+                target='_blank'
+                href='https://www.grubhub.com/restaurant/blosom-bento-735-e-12th-st-ste-411-los-angeles/283055'
+              >
+                <img
+                  src={grubhub}
+                  alt='order GrubHub'
+                  style={{ height: '2.95rem' }}
+                />
+              </Button>
+            </Col>
+            <Col xs='12' md='3'>
+              <Button
+                className='orderBtn'
+                outline
+                color='light'
+                target='_blank'
+                href='https://www.doordash.com/store/blosom-bento-los-angeles-32777/'
+              >
+                <img
+                  src={doordash}
+                  alt='order DoorDash'
+                  style={{ height: '2rem' }}
+                />
+              </Button>
+            </Col>
+            {/* <Col xs='12' md='3'>
+              <Button outline color='info'>
+                DOORDASH
+              </Button>
+            </Col> */}
           </Row>
         </div>
       </div>
